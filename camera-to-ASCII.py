3fe966cd560087a784ff3594aea214c4dfb689bf
lavnishhh <82657384+lavnishhh@users.noreply.github.com>
2021-11-30 11:48:47 +0530

    Monospace font support through PIL
    Since cv2 does not allow the use of monospace fonts, a part of code has been replaced with a PIL variant for monospace font support

--- camera-to-ASCII.py
+++ camera-to-ASCII.py
@@ -1,28 +1,34 @@
 import cv2
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+font = ImageFont.truetype("RobotoMono-Bold.ttf", 10)  
+
+print("import finised")
+
+image_size=512  #video ouput size for one dimesnion, other dimesnion is in ratio with second dimension of camera video
+read_size=40 #number of vertical lines of ASCII in each frame
+text=["#","=",":","."," "," "," "] #5 shades of grey, darkest 3 shades of grey are taken as the same shade
 
 vid = cv2.VideoCapture(0)
-image_size=512
-read_size=40
-text=["#","=",": ",". "]
-
 while(True):
         ret, frame = vid.read()
         frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         cv2.imshow('vid',frame)
         ratio=frame.shape[1]/frame.shape[0]
-        frame = cv2.resize(frame,dsize=(round(ratio*read_size),read_size), interpolation=cv2.INTER_CUBIC)
-        image = np.zeros((image_size,round(ratio*image_size)), np.uint8)
+        frame = cv2.resize(frame,dsize=(round(ratio*read_size)*2,read_size), interpolation=cv2.INTER_CUBIC)
+        img = Image.fromarray(np.zeros((image_size,round(ratio*image_size)), np.uint8))
         line=1
+        draw=ImageDraw.Draw(img)
+        draw.text((50,50),"hello",font=font,align="right")
         for a in frame:
                 col=""
                 for b in a:
                         col+=text[round((b/255)*len(text))-1]
-                image = cv2.putText(image, col, org=(0,round((line/read_size)*image_size)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255), thickness=1)
+                draw.text((0,round((line/read_size)*image_size)),col,font=font,align="left",fill=(255))
                 line=line+1
-        cv2.imshow('frame', image)
+        
+        cv2.imshow('frame', np.array(img))
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 vid.release()
 cv2.destroyAllWindows()
-
